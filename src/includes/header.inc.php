8d8518a26be44e93dfd1c48b2e892b3c4a2ce838
Matěj Košín <m.kosin1@seznam.cz>
2022-12-04 10:03:02 +0100

    Přidání ikony k panelu

--- src/includes/header.inc.php
+++ src/includes/header.inc.php
@@ -5,6 +5,7 @@ echo '<meta charset="UTF-8" />
     <link rel="stylesheet" href="./css/home-page.css" />
     <link rel="stylesheet" href="./css/all.min.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="./img/sps-logo.png" type="image/x-icon" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <title>Hlavní stránka</title>';
