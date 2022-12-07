efdb3b47d444a63646fea0d2aaca4e4a8a076db2
Michal Rychnovský <rychno07@student.vspj.cz>
2022-12-07 21:41:36 +0100

    Přidán recenzní formulář

--- src/recenze.php
+++ src/recenze.php
@@ -25,6 +25,52 @@
     <section>
       <div class="container">
         <h1>Recenze</h1>
+
+        <!--Šablona posudku -->
+        <div class="sablona">   
+          <h2>Šablona ke stažení</h2>
+          <a href="./sablony/recenzni-sablona.pdf"><i class=" fa-sharp fa-solid fa-file-word"></i></a>
+        </div>  
+
+        <!--Články k recenzi -->
+        <h3>Seznam článků přiřazených k hodnocení</h3>
+        <?php 
+          require_once 'includes/dbconn.inc.php';
+
+          $userId = $_SESSION["uzivatel_id"];
+          $sql = "SELECT clanek.id, clanek.nazev, uzivatel.jmeno, uzivatel.prijmeni, clanek.datum, stav.nazev as stav_nazev 
+                  FROM clanek 
+                  INNER JOIN uzivatel ON clanek.id_autor = uzivatel.id 
+                  INNER JOIN stav ON clanek.id_stav = stav.id
+                  WHERE clanek.id_recenzent = {$userId} AND stav.kod = 'odeslano_k_posudku';";
+
+            $result = mysqli_query($conn, $sql);
+          ?>
+
+           <table>
+            <tr>
+              <th>Název článku</th>
+              <th>Autor</th>
+              <th>Datum</th>
+              <th>Stav</th>
+            </tr>
+            
+            <?php 
+              if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_array($result)) {
+            ?>
+            
+            <tr>
+              <td><?php echo $row["nazev"]; ?></td>
+              <td><?php echo $row["jmeno"]." ".$row["prijmeni"]; ?></td>
+              <td><?php echo $row["datum"]; ?></td>
+              <td><?php echo $row["stav_nazev"]; ?></td>
+            </tr>
+
+            <?php
+                }
+              }
+            ?>
       </div>
     </section>
 
