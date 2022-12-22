a0786af662c9075e71e8343ddfc38817f23b3ddd
Lukas-Erber <114433178+Lukas-Erber@users.noreply.github.com>
2022-12-22 21:25:10 +0100

    Add files via upload

--- src/edit_save.php
+++ src/edit_save.php
@@ -12,5 +12,6 @@ mysqli_query($conn,"UPDATE uzivatel SET jmeno='".$jmeno."' WHERE id='".$id."'");
 mysqli_query($conn,"UPDATE uzivatel SET prijmeni='".$prijmeni."' WHERE id='".$id."'");
 mysqli_query($conn,"UPDATE uzivatel SET email='".$email."' WHERE id='".$id."'");
 mysqli_query($conn,"UPDATE uzivatel SET id_role='".$rolee."' WHERE id='".$id."'");
-header("Location: administrace.php");
+//header("Location: administrace.php");
+header("Refresh:0; url=administrace.php");
 ?>
