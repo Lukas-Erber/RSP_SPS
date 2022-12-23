c624bede32001120bca9baa393b05122dd2b68c3
Michal Rychnovský <rychno07@student.vspj.cz>
2022-12-23 11:32:24 +0100

    Zobrazení posudků pro sefredaktora

--- src/sefredaktor.php
+++ src/sefredaktor.php
@@ -9,25 +9,30 @@
   </head>
   <body>
     <?php include 'includes/main-bar.inc.php'; ?>
+    <?php include 'includes/posudek-modal.inc.php'; ?>
 
     <?php
         require_once 'includes/dbconn.inc.php';
         // Články vyžadující akci
-        $sql_clanky = "SELECT clanek.id, clanek.nazev as clanek_nazev, clanek.tema, clanek.datum, clanek.soubor, stav.nazev as stav_nazev, uzivatel.jmeno as autor_jmeno, uzivatel.prijmeni as autor_prijmeni, recenzent1.jmeno as recenzent_jmeno, recenzent1.prijmeni as recenzent_prijmeni, recenzent2.jmeno as recenzent2_jmeno, recenzent2.prijmeni as recenzent2_prijmeni 
+        $sql_clanky = "SELECT clanek.id, clanek.nazev as clanek_nazev, clanek.tema, clanek.datum, clanek.soubor, stav.nazev as stav_nazev, uzivatel.jmeno as autor_jmeno, uzivatel.prijmeni as autor_prijmeni, recenzent1.jmeno as recenzent_jmeno, recenzent1.prijmeni as recenzent_prijmeni, recenzent2.jmeno as recenzent2_jmeno, recenzent2.prijmeni as recenzent2_prijmeni, recenze1.aktualnost as r1_hod1, recenze1.zajimavost as r1_hod2, recenze1.prinosnost as r1_hod3, recenze1.originalita as r1_hod4, recenze1.odborna_uroven as r1_hod5, recenze1.jazykova_uroven as r1_hod6, recenze1.text as r1_text, recenze2.aktualnost as r2_hod1, recenze2.zajimavost as r2_hod2, recenze2.prinosnost as r2_hod3, recenze2.originalita as r2_hod4, recenze2.odborna_uroven as r2_hod5, recenze2.jazykova_uroven as r2_hod6, recenze2.text as r2_text
                        FROM clanek 
                        INNER JOIN uzivatel ON uzivatel.id = clanek.id_autor
                        INNER JOIN stav ON stav.id = clanek.id_stav AND stav.kod = 'odeslano_sefredaktorovi'
                        LEFT JOIN uzivatel AS recenzent1 ON recenzent1.id = clanek.id_recenzent  
-                       LEFT JOIN uzivatel AS recenzent2 ON recenzent2.id = clanek.id_recenzent2;";
+                       LEFT JOIN uzivatel AS recenzent2 ON recenzent2.id = clanek.id_recenzent2
+                       LEFT JOIN posudek as recenze1 ON recenze1.id_clanek = clanek.id AND recenze1.id_uzivatel = recenzent1.id
+                       LEFT JOIN posudek as recenze2 ON recenze2.id_clanek = clanek.id AND recenze2.id_uzivatel = recenzent2.id;";
         $result_clanky = mysqli_query($conn, $sql_clanky);
 
         // Články 
-        $sql_clanky_prehled = "SELECT clanek.nazev as clanek_nazev, clanek.tema, clanek.datum, clanek.soubor, stav.nazev as stav_nazev, uzivatel.jmeno as autor_jmeno, uzivatel.prijmeni as autor_prijmeni, recenzent1.jmeno as recenzent_jmeno, recenzent1.prijmeni as recenzent_prijmeni, recenzent2.jmeno as recenzent2_jmeno, recenzent2.prijmeni as recenzent2_prijmeni 
+        $sql_clanky_prehled = "SELECT clanek.nazev as clanek_nazev, clanek.tema, clanek.datum, clanek.soubor, stav.nazev as stav_nazev, uzivatel.jmeno as autor_jmeno, uzivatel.prijmeni as autor_prijmeni, recenzent1.jmeno as recenzent_jmeno, recenzent1.prijmeni as recenzent_prijmeni, recenzent2.jmeno as recenzent2_jmeno, recenzent2.prijmeni as recenzent2_prijmeni, recenze1.aktualnost as r1_hod1, recenze1.zajimavost as r1_hod2, recenze1.prinosnost as r1_hod3, recenze1.originalita as r1_hod4, recenze1.odborna_uroven as r1_hod5, recenze1.jazykova_uroven as r1_hod6, recenze1.text as r1_text, recenze2.aktualnost as r2_hod1, recenze2.zajimavost as r2_hod2, recenze2.prinosnost as r2_hod3, recenze2.originalita as r2_hod4, recenze2.odborna_uroven as r2_hod5, recenze2.jazykova_uroven as r2_hod6, recenze2.text as r2_text 
                        FROM clanek 
                        INNER JOIN uzivatel ON uzivatel.id = clanek.id_autor
                        INNER JOIN stav ON stav.id = clanek.id_stav
                        LEFT JOIN uzivatel AS recenzent1 ON recenzent1.id = clanek.id_recenzent  
-                       LEFT JOIN uzivatel AS recenzent2 ON recenzent2.id = clanek.id_recenzent2;";
+                       LEFT JOIN uzivatel AS recenzent2 ON recenzent2.id = clanek.id_recenzent2
+                       LEFT JOIN posudek as recenze1 ON recenze1.id_clanek = clanek.id AND recenze1.id_uzivatel = recenzent1.id
+                       LEFT JOIN posudek as recenze2 ON recenze2.id_clanek = clanek.id AND recenze2.id_uzivatel = recenzent2.id;";
         $result_clanky_prehled = mysqli_query($conn, $sql_clanky_prehled);
 
         // Autoři
@@ -82,8 +87,32 @@
                                 <td><?php echo $row["tema"]; ?></td>
                                 <td><?php echo $row["datum"]; ?></td>
                                 <td><?php echo $row["stav_nazev"]; ?></td>
-                                <td><?php echo $row["recenzent_jmeno"]." ".$row["recenzent_prijmeni"]; ?></td>
-                                <td><?php echo $row["recenzent2_jmeno"]." ".$row["recenzent2_prijmeni"]; ?></td>
+                                <td>
+                                    <a class="rec" data-toggle="modal" data-target="#recenzeModal" 
+                                            data-hod1="<?php echo $row['r1_hod1'] ?>" 
+                                            data-hod2="<?php echo $row['r1_hod2'] ?>" 
+                                            data-hod3="<?php echo $row['r1_hod3'] ?>"
+                                            data-hod4="<?php echo $row['r1_hod4'] ?>"
+                                            data-hod5="<?php echo $row['r1_hod5'] ?>"
+                                            data-hod6="<?php echo $row['r1_hod6'] ?>"
+                                            data-text="<?php echo $row['r1_text'] ?>"
+                                    >
+
+                                    <?php echo $row["recenzent_jmeno"]." ".$row["recenzent_prijmeni"]; ?></a>
+                                </td>
+                                <td>
+                                    <a class="rec" data-toggle="modal" data-target="#recenzeModal" 
+                                            data-hod1="<?php echo $row['r2_hod1'] ?>" 
+                                            data-hod2="<?php echo $row['r2_hod2'] ?>" 
+                                            data-hod3="<?php echo $row['r2_hod3'] ?>"
+                                            data-hod4="<?php echo $row['r2_hod4'] ?>"
+                                            data-hod5="<?php echo $row['r2_hod5'] ?>"
+                                            data-hod6="<?php echo $row['r2_hod6'] ?>"
+                                            data-text="<?php echo $row['r2_text'] ?>"
+                                    >
+
+                                    <?php echo $row["recenzent2_jmeno"]." ".$row["recenzent2_prijmeni"]; ?>  
+                                </td>
                                 <td>
                                   <a href="clanek_prijato.php?clanekId=<?php echo $row['id']; ?>">Přijmout</a><br>
                                   <a href="clanek_zamitnuto.php?clanekId=<?php echo $row['id']; ?>">Zamítnout</a><br>
@@ -127,8 +156,32 @@
                                 <td><?php echo $row["tema"]; ?></td>
                                 <td><?php echo $row["datum"]; ?></td>
                                 <td><?php echo $row["stav_nazev"]; ?></td>
-                                <td><?php echo $row["recenzent_jmeno"]." ".$row["recenzent_prijmeni"]; ?></td>
-                                <td><?php echo $row["recenzent2_jmeno"]." ".$row["recenzent2_prijmeni"]; ?></td>
+                                <td>
+                                    <a class="rec" data-toggle="modal" data-target="#recenzeModal" 
+                                            data-hod1="<?php echo $row['r1_hod1'] ?>" 
+                                            data-hod2="<?php echo $row['r1_hod2'] ?>" 
+                                            data-hod3="<?php echo $row['r1_hod3'] ?>"
+                                            data-hod4="<?php echo $row['r1_hod4'] ?>"
+                                            data-hod5="<?php echo $row['r1_hod5'] ?>"
+                                            data-hod6="<?php echo $row['r1_hod6'] ?>"
+                                            data-text="<?php echo $row['r1_text'] ?>"
+                                    >
+
+                                    <?php echo $row["recenzent_jmeno"]." ".$row["recenzent_prijmeni"]; ?></a>
+                                </td>
+                                <td>
+                                    <a class="rec" data-toggle="modal" data-target="#recenzeModal" 
+                                            data-hod1="<?php echo $row['r2_hod1'] ?>" 
+                                            data-hod2="<?php echo $row['r2_hod2'] ?>" 
+                                            data-hod3="<?php echo $row['r2_hod3'] ?>"
+                                            data-hod4="<?php echo $row['r2_hod4'] ?>"
+                                            data-hod5="<?php echo $row['r2_hod5'] ?>"
+                                            data-hod6="<?php echo $row['r2_hod6'] ?>"
+                                            data-text="<?php echo $row['r2_text'] ?>"
+                                    >
+
+                                    <?php echo $row["recenzent2_jmeno"]." ".$row["recenzent2_prijmeni"]; ?>  
+                                </td>
                             </tr>
 
                             <?php
@@ -251,11 +304,29 @@
     <?php include 'includes/footer.inc.php'; ?>
 
     <script type="text/javascript">
-                $(document).ready(function(){
-            $(".aaa").click(function(){
-                $("#clanek-id").val($(this).data("id"));
-            });
+      $(document).ready(function(){
+        $(".aaa").click(function(){
+          $("#clanek-id").val($(this).data("id"));
         });
+
+        $('.rec').click(function() {     
+                var text = $(this).data('text');  
+                var hod1 = $(this).data('hod1'); 
+                var hod2 = $(this).data('hod2');
+                var hod3 = $(this).data('hod3');
+                var hod4 = $(this).data('hod4');
+                var hod5 = $(this).data('hod5');
+                var hod6 = $(this).data('hod6');
+
+                $('#text').val(text);  
+                $('#hod1').val(hod1);  
+                $('#hod2').val(hod2);
+                $('#hod3').val(hod3);
+                $('#hod4').val(hod4);
+                $('#hod5').val(hod5);
+                $('#hod6').val(hod6);
+            });
+      });
     </script>
   </body>
 </html>
